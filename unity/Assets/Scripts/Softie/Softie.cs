80a80c27ba1cee5b2867621923aba59c9f1cc9f6
chongdashu <me@chongdashu.com>
2015-07-26 22:03:48 -0400

    Soft body wip.

---
+++ unity/Assets/Scripts/Softie/Softie.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Softie : MonoBehaviour {
+
+	public GameObject bearingPrefab;
+	public GameObject core;
+	public float bearingsPerAxis = 4;
+	public float bearingDistance = 1.0f;
+	public int numberOfAxes = 1;
+
+
+	// Use this for initialization
+	void Awake () 
+	{
+		for (int a=0; a < numberOfAxes; a++)
+		{
+			GameObject axisContainer = new GameObject();
+			axisContainer.transform.parent = this.transform;
+			axisContainer.name = "AxisContainer_" + a;
+
+			for (float i=0; i < bearingsPerAxis; i++)
+			{
+				float angle = 2 * Mathf.PI * (i/bearingsPerAxis);
+//				float angleDeg = Mathf.Rad2Deg *  (angle);
+				//			Debug.Log ("i=" + i + ", angleDeg= " + angleDeg);
+				float posX = bearingDistance * Mathf.Cos(angle);
+				float posY = bearingDistance * Mathf.Sin(angle);
+				
+				//			Debug.Log ("i=" + i + ", xPos=" + posX + ", yPos=" + posY);
+				
+				Vector3 corePos = core.transform.position;
+				
+				posX += core.transform.position.x;
+				posY += core.transform.position.y;
+				
+				GameObject bearingObject = Instantiate(bearingPrefab);
+				Vector3 bearingPosition = new Vector3( posX, posY, corePos.z);
+				bearingObject.transform.position = bearingPosition;
+				bearingObject.transform.parent = axisContainer.transform;
+
+
+			}
+			axisContainer.transform.Rotate(Vector3.up, a*180f/numberOfAxes);
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
