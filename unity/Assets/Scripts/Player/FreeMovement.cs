fe094c3d3c9f37ab7a122118f78d39d3686b367f
chongdashu <me@chongdashu.com>
2015-07-30 22:24:35 -0400

    Add movement based on camera
    Previously, the movement was based on the Input axis being mapped to
    player movement based on a fixed coordinate system of the world. This
    meant that regardless of the camera rotation, pressing right would make
    the player move in the same direction with respect to the map. This
    results in an unintuitive control scheme because players may choose to
    rotate the camera in any way that they please.
    
    This change thus bases the player's movement on the direction that the
    camera is facing. It uses the Camera.main.transform's facing directional
    vector and uses that to figure out the movement vector of the player.
    For pressing left and right keys (i.e., Input.Axis("Horizontal"), it
    rotates the movement vector by 90 degrees abouve the y-axis, and
    multiplies it by the respective coefficient. This is also the first time
    that I've used Quarternions for such a purpose, so Thank God that it
    worked the first time around!

--- unity/Assets/Scripts/Player/FreeMovement.cs
+++ unity/Assets/Scripts/Player/FreeMovement.cs
@@ -6,6 +6,7 @@ public class FreeMovement : MonoBehaviour {
 	public float movementSpeed = 4.0f;
 	public int floorMask;
 	public Vector3 movementVector;
+	public Vector3 normalizedMovementVector;
 	public Rigidbody rigidBody;
 
 
@@ -16,6 +17,7 @@ public class FreeMovement : MonoBehaviour {
 		floorMask = LayerMask.GetMask("TileGround");
 		rigidBody = GetComponent<Rigidbody>();
 		movementVector = Vector3.zero;
+		normalizedMovementVector = Vector3.zero;
 	}
 	
 	// Update is called once per frame
@@ -24,13 +26,31 @@ public class FreeMovement : MonoBehaviour {
 		float h = Input.GetAxisRaw("Horizontal"); 	// values {-1, 0, +1}
 		float v = Input.GetAxisRaw("Vertical");		// values {-1, 0, +1}
 
+
+
 		DoMove(h, v);
 	}
 
 	void DoMove(float h, float v)
 	{
-		movementVector.Set(h, 0, v);
-		movementVector = movementVector.normalized * movementSpeed * Time.deltaTime;
-		rigidBody.MovePosition(transform.position + movementVector);
+		if (h == 0 && v == 0) 
+		{
+			movementVector.Set(0f,0f,0f);
+		}
+		else
+		{
+			movementVector = Camera.main.transform.forward;
+		}
+
+		if (v != 0)
+		{
+			movementVector.Set(movementVector.x*v, 0f, movementVector.z*v);
+		}
+		if (h != 0)
+		{
+			movementVector = Quaternion.AngleAxis(90*h, Vector3.up) * movementVector;
+		}
+		normalizedMovementVector = movementVector.normalized * movementSpeed * Time.deltaTime;
+		rigidBody.MovePosition(transform.position + normalizedMovementVector);
 	}
 }
