29c1cc6767370734276e59c4b98638c51dabcadc
chongdashu <me@chongdashu.com>
2015-08-16 16:19:11 +0800

    Add enemy scripts to look at player

---
+++ unity/Assets/Scripts/Enemy/LookAtTarget.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtTarget : MonoBehaviour {
+
+	public Rigidbody rigidBody;
+	public Transform target;
+	public Vector3 targetPosition;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		this.rigidBody = this.GetComponent<Rigidbody>();
+	}
+
+	void Start() 
+	{
+		if (this.target != null)
+		{
+			this.targetPosition = this.target.transform.position;
+		}
+	}
+
+	void FixedUpdate () 
+	{
+		if (this.target != null)
+		{
+			this.targetPosition = this.target.position;
+		}
+		if (this.targetPosition != null)
+		{
+			Vector3 dir = this.targetPosition - this.transform.position;
+			Quaternion rotation = Quaternion.LookRotation(this.targetPosition - this.transform.position);
+			Debug.DrawRay(this.transform.position, dir, Color.blue);
+			this.transform.rotation = rotation;
+		}
+	}
+}
